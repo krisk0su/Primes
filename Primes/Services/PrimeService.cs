38f5aa7d41cbe115a229f1fa79c84ea5388c80cc
chrizkonf <chrizkoof@gmail.com>
2020-08-03 13:46:58 +0300

    speed optimization

--- Primes/Services/PrimeService.cs
+++ Primes/Services/PrimeService.cs
@@ -53,7 +53,7 @@
             if (n % 2 == 0 || n % 3 == 0)
                 return false;
 
-            for (long i = 5; long.MaxValue <= n; i = i + 6)
+            for (long i = 5; long.MaxValue <= n  || n*n <= n; i = i + 6)
                 if (n % i == 0 ||
                     n % (i + 2) == 0)
                     return false;
